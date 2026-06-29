aabc918ad3397b4fd26c69f9f12e7c9bd42eb728
CPUPetet <c.nwodo@abujaprep.com>
2026-06-29 17:43:46 +0100

    private.py

---
+++ KeepItPrivate.py
@@ -0,0 +1,14 @@
+class myClass:
+
+    __privateVar = 27;
+
+    def __privMeth(self):
+        print("I'm inside the class called myClass.")
+
+
+    def hello(self):
+        print("Private Variable Value: ", myClass.__privateVar)
+
+foo = myClass()   
+foo.hello()
+foo._myClass__privMeth()
--- config.py
+++
@@ -1,2 +0,0 @@
-HF_API_KEY = "enter_your_api_key_here"
-# Note: Paste API key in the double quotes later.
